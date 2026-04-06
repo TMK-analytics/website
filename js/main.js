9bcaa5333f69a4ade5cf47534b17fb1f05a02921
TMKanalytics <thao@tmkanalytics.com>
2026-04-06 14:29:48 -0400

    Adjust mobile flip card timer even higher

--- js/main.js
+++ js/main.js
@@ -160,7 +160,7 @@ flipCards.forEach(card => {
   card.addEventListener('touchend', (e) => {
     const dx = Math.abs(e.changedTouches[0].clientX - touchStartX);
     const dy = Math.abs(e.changedTouches[0].clientY - touchStartY);
-    if (dx < 20 && dy < 20) {
+    if (dx < 50 && dy < 50) {
       card.classList.toggle('flipped');
     }
   });
